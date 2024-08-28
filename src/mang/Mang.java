422a48c4a88b58ec88eb4a50a41bb68f74ded600
Acer <Acer@DESKTOP-0PALRG2>
2024-08-28 23:01:05 +0700

    Assignment 5

---
+++ src/mang/Mang.java
@@ -0,0 +1,12 @@
+
+package mang;
+public class Mang {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        for (int i = 0; i < 8; i++){
+            numbers[i] = 2 * i;
+            System.out.print(numbers[i]+" ");
+        }
+    }
+    
+}
